617c19ee50b86cd64f13ccdb6897d83211f3d352
Felix Loesing <loesing@usc.edu>
2021-02-22 14:59:05 -0800

    added non-root user to run elastic

--- sparkler-core/sparkler-deployment/docker/scripts/start.singleContainer.dev.sh
+++ sparkler-core/sparkler-deployment/docker/scripts/start.singleContainer.dev.sh
@@ -3,9 +3,9 @@
 # file is running as root
 
 # Start elasticsearch daemon
-/data/elasticsearch/bin/elasticsearch -d
+./elasticsearch/bin/elasticsearch -d
 
 # Start kibana daemon (-Q = no logging)
-/data/kibana/bin/kibana -Q &
+./kibana/bin/kibana -Q
 
 tail -f '/dev/null'
